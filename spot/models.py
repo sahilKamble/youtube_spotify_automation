ea7dd056b093792c58d6ba5127ad5af84981dd87
Genos2000 <paraskamble2000@gmail.com>
2020-03-22 00:45:07 +0530

    add Profile Model to admin

--- spot/models.py
+++ spot/models.py
@@ -8,6 +8,8 @@ class Profile(models.Model):
     creds = models.CharField(max_length=999, blank=True, null=True)
     gcreds = models.CharField(max_length=999, blank=True, null=True)
 
+    def __str__(self):
+        return self.user.username
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
